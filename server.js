055dd42fb27162834364b0612e5f2a6c9788f78a
Evandro Zanatta <hemanstifler@gmail.com>
2016-01-27 02:20:15 +0000

    Salvando desenvolvimento

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var tools = require('./user');
+var mysql = require("mysql");
+var async = require("async");
+
+var con = mysql.createConnection({
+  host: '0.0.0.0',
+  user: 'evandrozanatta',
+  database: 'smartlocation'
+});
+
+var start = new Date().getTime()
+
+con.connect();
 
 //Socket.io
 var io = require('socket.io')(http);
@@ -30,22 +42,35 @@ redisClient.on("error", function (err) {
 //Quando o cliente se conecta
 io.on('connection', function(socket){
     
-    console.log('## New Connection!');
-    
-    socket.on('connection', function(msg){
+    /*socket.on('connection', function(msg){
         io.emit('connection', "O usuário "+msg+" conectou-se.");
+    });*/
+    
+    socket.on('user_update', function(msg){
+        
+        con.query('SELECT COUNT(id) FROM users WHERE id = '+msg[0]+';', function(err, result, callback) {
+            console.log("Resultados no banco: "+result[0]['COUNT(id)']);
+            if(result[0]['COUNT(id)'] == 0){
+                insertUser();
+            }
+        });
+        
+        function insertUser(){
+            con.query('INSERT INTO users (id, name, login) VALUES ('+msg[0]+', ?, ?);', [msg[1], msg[2]], function(err, result){
+                returnOK();
+            });
+        }
+        
+        function returnOK(){
+            io.emit('user_signup', "O usuário "+msg[1]+" conectou-se.");
+        }
     });
 
 });
 
-io.on('connection', function(socket){
-    console.log('## New Connection!');
-    socket.on('newposition', function(position){
-        /*io.emit('chat message', msg);*/
-        console.log(position);
-    })
-});
-
+var stop = new Date().getTime();
+var total = (stop - start)/ 1000.0;
+console.log('Time = '+total);
 
 http.listen(process.env.PORT, function(){
     console.log('listening on *:'+process.env.PORT);
---
+++ smartlocation.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Máquina: 127.0.0.1
+-- Data de Criação: 26-Jan-2016 às 00:53
+-- Versão do servidor: 5.5.46-0ubuntu0.14.04.2
+-- versão do PHP: 5.5.9-1ubuntu4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de Dados: `smartlocation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sends`
+--
+
+CREATE TABLE IF NOT EXISTS `sends` (
+  `id_from` int(11) NOT NULL,
+  `id_to` int(11) NOT NULL,
+  `lat` float NOT NULL,
+  `long` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `login` text NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
